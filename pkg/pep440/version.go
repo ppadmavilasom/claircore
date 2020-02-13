d7396ae69555c3d362f4cbdbda5bbaa0170e2a37
Hank Donnay <hdonnay@redhat.com>
2020-02-13 15:47:49 -0500

    pep440: return claircore.Version

--- pkg/pep440/version.go
+++ pkg/pep440/version.go
@@ -8,6 +8,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/quay/claircore"
 )
 
 var pattern *regexp.Regexp
@@ -46,39 +48,52 @@ type Version struct {
 //
 // In generating this slice, the following rules are applied:
 //
-// Release is normalized to three numbers. Missing numbers are set to "0" and
+// Release is normalized to five numbers. Missing numbers are set to "0" and
 // additional numbers are dropped.
 //
 // The Dev revision is promoted earlier in the int slice if there's no Pre or
 // Post revision, and sorts as earlier than a Pre revision.
-func (v *Version) Version() []int {
+func (v *Version) Version() (c claircore.Version) {
+	const (
+		epoch = 0
+		rel   = 1
+		preL  = 6
+		preN  = 7
+		post  = 8
+		dev   = 9
+	)
 	// BUG(hank) The int-slice versioning method tries to accomdate arbitrary
 	// numbers, but may give odd results with sufficiently large revision
 	// numbers. One suggested workaround is to make fewer than 9 quintillion
 	// releases.
-	r := make([]int, 8)
-	r[0] = v.Epoch
-	copy(r[1:], v.Release)
+	c.Kind = "pep440"
+	c.V[epoch] = int32(v.Epoch)
+	for i, n := range v.Release {
+		if i > 4 {
+			break
+		}
+		c.V[rel+i] = int32(n)
+	}
 	switch v.Pre.Label {
 	case "a":
-		r[4] = -3
+		c.V[preL] = -3
 	case "b":
-		r[4] = -2
+		c.V[preL] = -2
 	case "rc":
-		r[4] = -1
+		c.V[preL] = -1
 	}
-	r[5] = v.Pre.N
-	r[6] = v.Post
+	c.V[preN] = int32(v.Pre.N)
+	c.V[post] = int32(v.Post)
 	if v.Dev != 0 {
-		if v.Post != 0 || r[4] != 0 {
-			r[7] = -v.Dev
+		if v.Post != 0 || c.V[preL] != 0 {
+			c.V[dev] = -int32(v.Dev)
 		} else {
-			const minInt = -int((^uint(0))>>1) - 1
-			r[4] = minInt + v.Dev
+			const minInt = -int32((^uint32(0))>>1) - 1
+			c.V[preL] = minInt + int32(v.Dev)
 		}
 	}
 
-	return r
+	return c
 }
 
 // String returns the canonicalized representation of the Version.
@@ -109,16 +124,8 @@ func (v *Version) String() string {
 // Compare returns an integer comparing two versions. The result will be 0 if
 // a == b, -1 if a < b and +1 if a > b.
 func (a *Version) Compare(b *Version) int {
-	as, bs := a.Version(), b.Version()
-	for k := range as {
-		if as[k] > bs[k] {
-			return 1
-		}
-		if as[k] < bs[k] {
-			return -1
-		}
-	}
-	return 0
+	av, bv := a.Version(), b.Version()
+	return av.Compare(&bv)
 }
 
 // Parse attempts to extract a PEP-440 version string from the provided string.
